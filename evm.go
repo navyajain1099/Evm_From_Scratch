f68c4b206bd841e50f3741a2a0f826c84134d950
navyajain1099 <navyajain1099@gmail.com>
2025-07-20 09:26:56 +0530

    add , sub , div , mul operators opcode

--- evm.go
+++ evm.go
@@ -57,8 +57,54 @@ func Evm(code []byte) ([]*big.Int, bool) {
 			// 	value := (int64(code[pc+1]) << 24) | (int64(code[pc+2]) << 16) | int64(code[pc+3])<<8 | int64(code[pc+4])
 			// 	stack = append(stack, big.NewInt(value))
 			// 	pc += 4 // Increment pc to skip the next four bytes
+		case 0x50: // POP
+			if len(stack) == 0 {
+				return stack, false // Stack underflow
+			}
+			stack = stack[1:] // Remove the top element from the stack
+		case 0x01: // ADD
+			if len(stack) < 2 {
+				return stack, false // Stack underflow
+			}
+			value1 := stack[0]
+			value2 := stack[1]
+			stack = stack[2:]                              // Remove the top two elements from the stack
+			sumValue := new(big.Int).Add(value1, value2)   // Add the two values
+			stack = append([]*big.Int{sumValue}, stack...) // Push the sum back onto the stack
+		case 0x02: // MUL
+			if len(stack) < 2 {
+				return stack, false // Stack underflow
+			}
+			value1 := stack[0]
+			value2 := stack[1]
+			stack = stack[2:]
+			productValue := new(big.Int).Mul(value1, value2)   // Multiply the two values
+			stack = append([]*big.Int{productValue}, stack...) // Push the product back onto the stack
+		case 0x03: // SUB
+			if len(stack) < 2 {
+				return stack, false // Stack underflow
+			}
+			value1 := stack[0]
+			value2 := stack[1]
+			stack = stack[2:]
+			subtractValue := new(big.Int).Sub(value1, value2)   // Subtract the two values
+			stack = append([]*big.Int{subtractValue}, stack...) // Push the difference back onto the stack
+		case 0x04: // DIV
+			if len(stack) < 2 {
+				return stack, false // Stack underflow
+			}
+
+			if stack[1].Sign() == 0 {
+				return stack, false // Division by zero
+			}
+			value1 := stack[0]
+			value2 := stack[1]
+			stack = stack[2:]
+			divValue := new(big.Int).Div(value1, value2)   // Divide the two values
+			stack = append([]*big.Int{divValue}, stack...) // Push the quotient back onto the stack
 
 		}
+
 	}
 	return stack, true
 }
